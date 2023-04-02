2136ccf2ab3637ed31410e1e78b3c6a11d930b11
jcarriong <finales.15@gmail.com>
2023-04-02 01:32:50 -0500

    registrando config client y eureka discover client

--- src/main/java/com/bootcamp/bankcustomer/BankCustomerApplication.java
+++ src/main/java/com/bootcamp/bankcustomer/BankCustomerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 @EnableMongoAuditing
 public class BankCustomerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankCustomerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankCustomerApplication.class, args);
+    }
 
 }
